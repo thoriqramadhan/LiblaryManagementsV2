9b778ea8c88494435f9a2331af6eb278ac87070a
thoriqramadhan <respect.thoriq@gmail.com>
2024-08-13 14:05:12 +0700

    feat: add responsive layout structure for dashboard

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,20 +1,65 @@
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head } from '@inertiajs/react';
+import Dropdown from "@/Components/Dropdown";
+import Table from "@/Components/Table";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { Head } from "@inertiajs/react";
 
 export default function Dashboard({ auth }) {
     return (
         <AuthenticatedLayout
             user={auth.user}
-            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>}
+            header={
+                <h2 className="font-semibold text-xl text-gray-800 leading-tight">
+                    Dashboard
+                </h2>
+            }
         >
             <Head title="Dashboard" />
 
-            <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">You're logged in!</div>
+            <div className="relative h-[100vh] sm:flex">
+                {/* sidebar */}
+                <div className="w-full h-[80px] bg-red-100 fixed bottom-0 flex justify-around items-center sm:static sm:h-full sm:w-20 sm:flex-col sm:justify-start sm:gap-y-4 sm:py-[40px] sm:overflow-hidden">
+                    <div className="p-2 cursor-pointer bg-slate-700 text-white hover:bg-black/50">
+                        List Book
+                    </div>
+                    <div className="p-2 cursor-pointer hover:bg-black/50">
+                        Borrowed
                     </div>
                 </div>
+                {/* content */}
+                <main className="h-full bg-slate-500 mb-[80px] pt-10  sm:flex-1 sm:px-10 sm:py-5 flex flex-col gap-y-7">
+                    <div className="px-5 flex items-center justify-between">
+                        <Dropdown>
+                            <Dropdown.Trigger>
+                                <span className="p-2 font-semibold bg-white rounded-md border px-4 py-2 text-start text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out">
+                                    Genre
+                                </span>
+                            </Dropdown.Trigger>
+
+                            <Dropdown.Content align="left">
+                                <Dropdown.Link href="#">
+                                    <span>Test</span>
+                                    <span className="ml-[110px] inline-block">
+                                        01
+                                    </span>
+                                </Dropdown.Link>
+                            </Dropdown.Content>
+                        </Dropdown>
+
+                        <div className="flex gap-x-3">
+                            <div className="bg-white rounded-md border px-4 py-2 text-start text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out">
+                                Available
+                            </div>
+                            <div className="bg-white rounded-md border px-4 py-2 text-start text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out">
+                                Booked
+                            </div>
+                        </div>
+                    </div>
+                    <div className="w-full flex-1 bg-red-800 px-4 py-4">
+                        <p>List Of Books</p>
+                        <p>Description testt </p>
+                        <Table />
+                    </div>
+                </main>
             </div>
         </AuthenticatedLayout>
     );
