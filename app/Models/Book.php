224f44f8a4dbd6df0685dcaaa6bfab7b8e3afd06
thoriqramadhan <respect.thoriq@gmail.com>
2024-08-13 14:31:10 +0700

    feat: make relation for columns

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Book extends Model
 {
@@ -11,5 +13,8 @@ class Book extends Model
 
     public function user(){
         return $this->belongsTo(User::class);
+    }
+    public function category(){
+        return $this->belongsTo(Category::class);
     }
 }
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,10 +2,15 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Book;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Category extends Model
 {
     use HasFactory;
+
+    public function book(){
+        $this->hasMany(Book::class);
+    }
 }
