f43fb6e655b8811ddb44e1f70f97873e1c68d43f
thoriqramadhan <respect.thoriq@gmail.com>
2024-08-14 10:58:05 +0700

    feat: get data for get method

---
+++ app/Http/Controllers/DashboardResource.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book;
+use App\Models\User;
+use Illuminate\Container\Attributes\Auth;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class DashboardResource extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $user = auth()->user()->load('book');
+        // dd($user);
+        return Inertia::render('Dashboard', [
+            'books' => Book::all()
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Book $book)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Book $book)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Book $book)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Book $book)
+    {
+        //
+    }
+}
