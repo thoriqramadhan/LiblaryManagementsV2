7a6711a70ca00994d314654f818cbbeec80cf5d1
thoriqramadhan <respect.thoriq@gmail.com>
2024-08-15 14:28:24 +0700

    feat: make components for sidebar

--- resources/js/Components/Sidebar.jsx
+++ resources/js/Components/Sidebar.jsx
@@ -1,27 +1,45 @@
 import React from "react";
+import { HomeSvg, ListSvg } from "./Svg";
+import SidebarTab from "./SidebarTab";
 
 function Sidebar({ setTab, selectedTab, setSelectedTab }) {
     function setTab(param, callback) {
         callback(param);
     }
     return (
-        <div className="w-full h-[80px] bg-red-100 fixed bottom-0 flex justify-around items-center sm:static sm:h-full sm:w-20 sm:flex-col sm:justify-start sm:gap-y-4 sm:py-[40px] sm:overflow-hidden">
-            <div
-                className={`p-2 cursor-pointer ${
-                    selectedTab == "book-lists" ? "selected-tab" : ""
-                }`}
-                onClick={() => setTab("book-lists", setSelectedTab)}
+        <div className="z-10 w-full h-[80px] bg-white border-t shadow-md fixed bottom-0 flex justify-around items-center sm:static sm:h-full sm:w-20 sm:flex-col sm:justify-start sm:gap-y-4 sm:py-[40px] sm:overflow-hidden">
+            <SidebarTab
+                selectedTab={selectedTab}
+                setSelectedTab={setSelectedTab}
+                setTab={setTab}
+                using="book-lists"
             >
-                List Book
-            </div>
-            <div
-                className={`p-2 cursor-pointer ${
-                    selectedTab == "booked-lists" ? "selected-tab" : ""
-                }`}
-                onClick={() => setTab("booked-lists", setSelectedTab)}
+                <span className="group-hover:text-white  sm:hidden">
+                    List Book
+                </span>
+                <HomeSvg
+                    color="white"
+                    height="25"
+                    width="25"
+                    className="hidden sm:inline-block"
+                />
+            </SidebarTab>
+            <SidebarTab
+                setTab={setTab}
+                selectedTab={selectedTab}
+                setSelectedTab={setSelectedTab}
+                using="booked-lists"
             >
-                Borrowed
-            </div>
+                <span className="group-hover:text-white sm:hidden">
+                    Borrowed
+                </span>
+                <ListSvg
+                    height="25"
+                    width="25"
+                    color="white"
+                    className="hidden sm:inline-block"
+                />
+            </SidebarTab>
         </div>
     );
 }
